4959c1d93e256b04cf1722a87f9e679a510d4cf1
jcejudo <joseed.cejudo@europeana.eu>
2024-04-23 14:32:39 +0200

    added parallelelization to building labeled dataset

--- scripts/get_labeled_dataset.py
+++ scripts/get_labeled_dataset.py
@@ -2,10 +2,12 @@ import os
 import time
 import fire
 from pathlib import Path
-
+from tqdm import tqdm
 import pandas as pd
 import pyeuropeana.apis as apis
 import pyeuropeana.utils as utils
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 
 
 def search_dataset(dataset_name, rows):
@@ -19,37 +21,46 @@ def search_dataset(dataset_name, rows):
       )
   return utils.search2df(response)
 
+def search_and_append(results, dataset_name, category, rows_dict):
+    response = search_dataset(dataset_name, rows_dict[category])
+    response['category'] = category[:-1] 
+    results.append(response)
+
 def main(**kwargs):
-  input_path = kwargs.get("input_path")
-  output_path = kwargs.get("output_path")
+    input_path = kwargs.get("input_path")
+    output_path = kwargs.get("output_path")
 
-  print("Getting training dataset...")
-  start = time.time()
+    print("Getting training dataset...")
+    start = time.time()
 
-  df = pd.read_csv(input_path)
-  df = df.drop_duplicates(subset=['dataset_name'], keep = "last")
+    df = pd.read_csv(input_path)
+    df = df.drop_duplicates(subset=['dataset_name'], keep="last")
 
-  cat_dict = {'watermarks':'Full','no_watermarks':'None'}
-  rows_dict = {'watermarks':400,'no_watermarks':100}
-  object_df = pd.DataFrame()
-  for category in cat_dict.keys():
-    print(category)
-    cat_df = df.loc[df['watermarks'] == cat_dict[category]]
-    total = cat_df.shape[0]
-    print(f"Number of datasets: {total}")
-    for i,dataset_name in enumerate(cat_df['dataset_name'].values[:]):
-      response = search_dataset(dataset_name, rows_dict[category])
-      response['category'] = category[:-1]
-      object_df = pd.concat([object_df,response])
-      if i>0 and i%5 == 0:
-        print(f'{round(100*i/total,1)}%')
+    cat_dict = {'watermarks': 'Full', 'no_watermarks': 'None'}
+    rows_dict = {'watermarks': 400, 'no_watermarks': 100}
 
-  object_df.to_csv(output_path,index = False)
+    object_df = pd.DataFrame()
+    results = []
 
-  end = time.time()
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        future_to_ds = {executor.submit(search_and_append, results, dataset_name, category, rows_dict): dataset_name 
+                        for category in cat_dict.keys() 
+                        for dataset_name in df.loc[df['watermarks'] == cat_dict[category]]['dataset_name'].values}
 
-  dt = (end-start)/60
-  print(f"Finished, it took {dt} minutes")
+        for future in tqdm(as_completed(future_to_ds), total=len(future_to_ds), desc="Searching Datasets"):
+            dataset_name = future_to_ds[future]
+            try:
+                future.result() 
+            except Exception as exc:
+                print(f'{dataset_name} generated an exception: {exc}')
+
+    object_df = pd.concat(results, ignore_index=True)
+    object_df.to_csv(output_path, index=False)
+
+    end = time.time()
+    dt = (end - start) / 60
+    print(f"Finished, it took {dt:.2f} minutes")
+
 
 
 if __name__ == "__main__":
